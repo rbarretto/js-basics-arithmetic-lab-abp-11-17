7051e4e9885bd21ffb16f8f7393563a40d4bf048
rbarretto <richard.a.barretto@gmail.com>
2018-02-03 04:57:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,4 @@ You should NOT try to redefine any of the above variables.
 newID = (1000000000 + oldID)
 ageIsValid = Number.isInteger(currentAge)
 randomNumber = Math.floor(Math.random() * 20) + 0
+randomInteger = Math.round(randomNumber)
